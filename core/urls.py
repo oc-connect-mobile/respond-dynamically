57df4dbb7faddc18545d101df4989f2389a776c7
AlanaKaimi <ajbryce@live.com>
2019-04-16 12:38:46 -0400

    Bare-bones city-search form.

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
    path('', views.index, name='index'), 
+   path('city-search/', views.city_form, name='city-search'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+# Our App imports:
+from core.forms import PleaseSearchForm
+from core.models import PleaseSearch
 # Create your views here.
 
 
@@ -11,4 +14,22 @@ def index(request):
         # 'context variable': context variable,
     })
     return response
+
+
+def city_form(request):
     
+    if request.method == 'GET':
+        search_city_form = PleaseSearchForm(request.GET)
+        
+        
+        if search_city_form.is_valid():
+            city = search_city_form.save(commit=False)
+            
+            return redirect(deck.get_absolute_url())
+
+    else:
+        search_city_form = PleaseSearchForm
+            
+    return render(request, 'city-search.html', {
+        'search_city_form': search_city_form,
+    })

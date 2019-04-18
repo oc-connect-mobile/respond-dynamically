f9c23001570ed6cf5499946d6667a96e9912ee35
Jay Spain <annakirklandsmith@gmail.com>
2019-04-18 12:57:11 -0400

    adding secondary choices

--- core/models.py
+++ core/models.py
@@ -35,6 +35,8 @@ class PleaseSearch(models.Model):
     query_string_start = models.CharField(max_length=100)
     query_string_end = models.CharField(max_length=100)
     sf_field = models.CharField(max_length=100)
+    total_search_record = models.CharField(max_length=750, default="none")
+    city_search_record = models.CharField(max_length=500, default="none")
 
     CATEGORY_CHOICES = (
         ('Any category', 'Any category'),
@@ -54,6 +56,7 @@ class PleaseSearch(models.Model):
     )
 
     categories = models.CharField(max_length=25, choices=CATEGORY_CHOICES, null=False)
+    category_search_record = models.CharField(max_length=500, default="none")
 
     COUNTY_CHOICES = (
         ('Any county', 'Any county'),
@@ -67,7 +70,110 @@ class PleaseSearch(models.Model):
     )
 
     counties = models.CharField(max_length=25, choices=COUNTY_CHOICES, null=False)
+    county_search_record = models.CharField(max_length=500, default="none")
 
+    SECONDARY_CHOICES = (
+        ('Any secondary', 'Any secondary'),
+        ('Addiction & Recovery', 'Addiction & Recovery'),
+        ('Adult Daycare', 'Adult Daycare'),
+        ('Advocacy & Legal Aid', 'Advocacy & Legal Aid'),
+        ('Affordable Housing', 'Affordable Housing'),
+        ('Alternative Medicine', 'Alternative Medicine'),
+        ('Assisted Living', 'Assisted Living'),
+        ('Baby Supplies', 'Baby Supplies'),
+        ('Birth Control', 'Birth Control'),
+        ('Blankets & Fans', 'Blankets & Fans'),
+        ('Books', 'Books'),
+        ('Bus Passes', 'Bus Passes'),
+        ('Busses', 'Busses'),
+        ('Checkup & Test', 'Checkup & Test'),
+        ('Childcare', 'Childcare'),
+        ('Clothes for School', 'Clothes for School'),
+        ('Clothing', 'Clothing'),
+        ('Computer Class', 'Computer Class'),
+        ('Counseling', 'Counseling'),
+        ('Credit Counseling', 'Credit Counseling'),
+        ('Dental Care', 'Dental Care'),
+        ('Diapers &', 'Diapers &'),
+        ('Disaster Response', 'Disaster Response'),
+        ('Discounted Healthcare', 'Discounted Healthcare'),
+        ('Domestic Violence Issues', 'Domestic Violence Issues'),
+        ('Emergency Food', 'Emergency Food'),
+        ('Emergency Money', 'Emergency Money'),
+        ('Emergency Shelter', 'Emergency Shelter'),
+        ('English as a Second Language (ESL)', 'English as a Second Language (ESL)'),
+        ('Financial Assistance', 'Financial Assistance'),
+        ('Financial Education', 'Financial Education'),
+        ('Flood Relief', 'Flood Relief'),
+        ('Food Pantry', 'Food Pantry'),
+        ('Foreign Languages', 'Foreign Languages'),
+        ('Free Meals', 'Free Meals'),
+        ('Furniture', 'Furniture'),
+        ('GED/High-School Equivalency', 'GED/High-School Equivalency'),
+        ('Health Educ', 'Health Educ'),
+        ('Health Education', 'Health Education'),
+        ('Help Escape Violence', 'Help Escape Violence'),
+        ('Help Fill out Forms', 'Help Fill out Forms'),
+        ('Help Find Chi', 'Help Find Chi'),
+        ('Help Find Housing', 'Help Find Housing'),
+        ('Help Find Work', 'Help Find Work'),
+        ('Help Hotlines', 'Help Hotlines'),
+        ('Help Pay for Childcare', 'Help Pay for Childcare'),
+        ('Help Pay for Healthcare', 'Help Pay for Healthcare'),
+        ('Help Pay for Housing', 'Help Pay for Housing'),
+        ('Help Pay for Rent', 'Help Pay for Rent'),
+        ('Help Pay for School', 'Help Pay for School'),
+        ('Help Pay for Utilities', 'Help Pay for Utilities'),
+        ('Help Pay for Vehicles', 'Help Pay for Vehicles'),
+        ('Home Goods', 'Home Goods'),
+        ('Homebuyer Education', 'Homebuyer Education'),
+        ('Housing Advice', 'Housing Advice'),
+        ('Housing Relief', 'Housing Relief'),
+        ('Immediate Safety', 'Immediate Safety'),
+        ('Independent Living', 'Independent Living'),
+        ('Insurance', 'Insurance'),
+        ('Long-Term Housing', 'Long-Term Housing'),
+        ('Maternity Care', 'Maternity Care'),
+        ('Medical Care', 'Medical Care'),
+        ('Medical Supplies', 'Medical Supplies'),
+        ('Mental Health', 'Mental Health'),
+        ('Mentoring', 'Mentoring'),
+        ('Money Management', 'Money Management'),
+        ('Navigating the System', 'Navigating the System'),
+        ('Nursing Home', 'Nursing Home'),
+        ('Nutrition', 'Nutrition'),
+        ('Outpatient Treatment', 'Outpatient Treatment'),
+        ('Personal Care Items', 'Personal Care Items'),
+        ('Personal Hygiene', 'Personal Hygiene'),
+        ('Prescription Assistance', 'Prescription Assistance'),
+        ('Prevent & Treat', 'Prevent & Treat'),
+        ('Primary Care', 'Primary Care'),
+        ('Psychiatric Emergency', 'Psychiatric Emergency'),
+        ('Public Housing', 'Public Housing'),
+        ('Representation', 'Representation'),
+        ('Resume Development', 'Resume Development'),
+        ('Screening & Exams', 'Screening & Exams'),
+        ('Skills & Training', 'Skills & Training'),
+        ('Specialized Ther', 'Specialized Ther'),
+        ('Specialized Training', 'Specialized Training'),
+        ('Supplies for Work', 'Supplies for Work'),
+        ('Support Group', 'Support Group'),
+        ('Support Network', 'Support Network'),
+        ('Tax Preparation', 'Tax Preparation'),
+        ('Toys & Gift', 'Toys & Gift'),
+        ('Transportation', 'Transportation'),
+        ('Virtual Support', 'Virtual Support'),
+        ('Vision Care', 'Vision Care'),
+        ('Vision Tests', 'Vision Tests'),
+        ('Workplace Rights', 'Workplace Rights'),
+    )
+
+    secondaries = models.CharField(max_length=50, choices=SECONDARY_CHOICES, default="none")
+    secondary_search_record = models.CharField(max_length=500, default="none")
+
+    class LuckySearch(models.Model):
+        lucky_string = models.CharField(max_length=100)
+        searched_at = models.DateTimeField(auto_now_add=True)
 
     
 

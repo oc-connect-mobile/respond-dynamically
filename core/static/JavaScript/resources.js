cc0882135fedad34d26d3d4a2e784abb26493935
Jay Spain <annakirklandsmith@gmail.com>
2019-04-24 18:12:19 -0400

    added tooltips to icons

--- core/static/JavaScript/resources.js
+++ core/static/JavaScript/resources.js
@@ -168,7 +168,7 @@ function populateList(resources, idx){
           categoryTag.innerText = catList[i]
           categoryTag.classList.add('listed-cat', (`${cat}`))
           let iconName = addIconToCategory (cat)
-          categoryTag.innerHTML = `<i class="material-icons i-${lowerCat}" aria-label="${cat}" aria-hidden="true">${iconName}</i>`
+          categoryTag.innerHTML = `<i class="material-icons i-${lowerCat}" title="${cat}" aria-label="${cat}" aria-hidden="true">${iconName}</i>`
           categoryList.appendChild(categoryTag)
         }
       }
@@ -178,7 +178,7 @@ function populateList(resources, idx){
         categoryTag.innerText = catList
         categoryTag.classList.add('listed-cat', (`${cat}`))
         let iconName = addIconToCategory (cat)
-        categoryTag.innerHTML = `<i class="material-icons i-${lowerCat}" aria-label="${cat}" aria-hidden="true">${iconName}</i>`
+        categoryTag.innerHTML = `<i class="material-icons i-${lowerCat}" title="${cat}" aria-label="${cat}" aria-hidden="true">${iconName}</i>`
         categoryList.appendChild(categoryTag, categoryTag)
       }
     }

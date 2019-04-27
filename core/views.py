88bedaecb5709f287bad859b2bd2d89ec5ff1151
Jay Spain <annakirklandsmith@gmail.com>
2019-04-27 17:31:50 -0400

    styling and query change

--- core/views.py
+++ core/views.py
@@ -20,7 +20,7 @@ from django import forms
 def index(request):
     """View function for home page of site."""
     """ url = "/services/data/v45.0/" """
-    a = "query?q=SELECT+ID,+Name,+CEF_Category__c,+County_Served__c,+City_Served__c,+Website,+Eligibility_Criteria__c,+CEF_Sub_Category__c,+Secondary_Tags__c,+Imported_Phone__c,+Company_Email__c,+Description_Short__c+FROM+Account"
+    a = "query?q=SELECT+ID,+Name,+CEF_Category__c,+County_Served__c,+City_Served__c,+Website,+Eligibility_Criteria__c,+CEF_Sub_Category__c,+Secondary_Tags__c,+Imported_Phone__c,+Company_Email__c,+Description_Short__c,Primary_City__c+FROM+Account"
     b = "+WHERE+"
     x = "Deactivated__c=FALSE"
     y= "+ORDER+BY+Website"

7542e944eac0a2ecddc0d448d020d592e13cfe5c
AlanaKaimi <ajbryce@live.com>
2019-04-28 16:44:35 -0400

    updating styles

--- core/views.py
+++ core/views.py
@@ -23,7 +23,7 @@ def index(request):
     a = "query?q=SELECT+ID,+Name,+CEF_Category__c,+County_Served__c,+City_Served__c,+Website,+Eligibility_Criteria__c,+CEF_Sub_Category__c,+Secondary_Tags__c,+Imported_Phone__c,+Company_Email__c,+Description_Short__c,Primary_City__c+FROM+Account"
     b = "+WHERE+"
     x = "Deactivated__c=FALSE"
-    y= "+ORDER+BY+Website"
+    y= "+ORDER+BY+Website+NULLS+LAST"
     #+NULLS+LAST+LIMIT+3"
 
     r = "parameterizedSearch/?q="
